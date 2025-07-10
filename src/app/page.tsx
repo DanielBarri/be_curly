b042d7129c1d6ba71cefbecfaff41cf7a7d2e55b
Daniel Barri <daniel.barri@gmail.com>
2025-07-10 12:32:59 -0600

    Added animation to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
+import SplashCursor from "@/components/SplashCursor";
 
 export default function Home() {
     return (
-        <div className="w-screen h-screen">
-            <header className="flex flex-row w-full justify-end absolute">
-                <Navbar />
-            </header>
-            <div className="flex h-full items-center justify-center">
-                <Image
-                    src="/logo.svg"
-                    alt="logo image"
-                    width={400}
-                    height={400}
-                />
+        <>
+            <SplashCursor />
+            <div className="h-full w-full relative">
+                <header className="w-full flex flex-row justify-end absolute">
+                    <Navbar />
+                </header>
+                <div className="flex bg-becurlyBlack h-full items-center justify-center">
+                    <Image
+                        src="/whiteLogo.svg"
+                        alt="logo image"
+                        width={400}
+                        height={400}
+                    />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,55 @@
 import Link from "next/link";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBottleDroplet,
+    faGraduationCap,
+    faShower,
+} from "@fortawesome/free-solid-svg-icons";
+
+const menuOptions = [
+    {
+        title: "Tutoriales",
+        url: "/guides",
+        icon: faGraduationCap,
+    },
+    {
+        title: "Rutina",
+        url: "/routines",
+        icon: faShower,
+    },
+    {
+        title: "Productos",
+        url: "/products",
+        icon: faBottleDroplet,
+    },
+];
 
 const Menu = () => {
     return (
-        <div className="">
-            <Link href="/" className="flex items-center justify-center gap-4">
-                <Image src="/logo.svg" alt="logo" width={150} height={150} />
+        <div className="bg-becurlyBlack rounded-xl h-full flex flex-col items-center text-white pt-2">
+            <Link
+                href="/"
+                className="flex flex-col items-center justify-center gap-4">
+                <Image
+                    src="/shortLogoWhite.svg"
+                    alt="logo"
+                    width={150}
+                    height={150}
+                />
             </Link>
+            <nav className="flex flex-col justify-center gap-6 mt-8">
+                {menuOptions.map((option) => (
+                    <Link
+                        key={option.title}
+                        href={option.url}
+                        className="text-lg hover:text-pink-300 hover:text-xl transition duration-150 ease-in-out">
+                        {option.title}
+                        &nbsp; &nbsp;
+                        <FontAwesomeIcon icon={option.icon} size="lg" />
+                    </Link>
+                ))}
+            </nav>
         </div>
     );
 };
